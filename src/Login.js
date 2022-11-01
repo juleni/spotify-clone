d2bbc50092ccecc6d1f20b9f9ee857230b56a63f
juleni <69912179+juleni@users.noreply.github.com>
2022-11-01 12:26:00 +0100

    Initial commit

---
+++ src/Login.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const AUTH_URL =
+  "https://accounts.spotify.com/authorize?client_id=521c7d7095d4429983c8208e90694c24&response_type=code&redirect_uri=http://localhost:3000&&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-write-modify%20user-read-playback-state%20user-modify-playback-state";
+
+export default function Login() {
+  return <div>Login</div>;
+}
