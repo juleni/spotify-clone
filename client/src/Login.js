2b6bd97cb0e52bffa9526eb4a3f4a7e2bd3b25fa
juleni <69912179+juleni@users.noreply.github.com>
2022-11-01 14:15:53 +0100

    Added client and server root directories + replaced files into them

--- client/src/Login.js
+++ client/src/Login.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Container } from "react-bootstrap";
 
 const AUTH_URL =
-  "https://accounts.spotify.com/authorize?client_id=521c7d7095d4429983c8208e90694c24&response_type=code&redirect_uri=http://localhost:3000&&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-write-modify%20user-read-playback-state%20user-modify-playback-state";
+  "https://accounts.spotify.com/authorize?client_id=521c7d7095d4429983c8208e90694c24&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-currently-playing%20user-read-playback-position%20playlist-modify-private%20playlist-modify-public";
 
 export default function Login() {
   return (
