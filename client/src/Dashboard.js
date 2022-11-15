2b5db4c198046ce283e25be16863351183084df3
juleni <69912179+juleni@users.noreply.github.com>
2022-11-15 16:14:19 +0100

    Added search/songs/Bottom parts

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -1,5 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import { Container, Form } from "react-bootstrap";
+import useAuth from "./useAuth";
 
 export default function Dashboard({ code }) {
-  return <div>Dashboard code: {code}</div>;
+  const accessToken = useAuth(code);
+  const [search, setSearch] = useState("");
+  console.log("Dashboard CODE = " + code);
+  console.log("Dashboard accessToken = " + accessToken);
+  return (
+    <Container className="d-flex flex-column py-2" style={{ height: "100vh" }}>
+      <Form.Control
+        type="search"
+        placeholder="Search Songs/Artists"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <div className="flex-grow-1 my-2" style={{ overflowY: "auto" }}>
+        Songs
+      </div>
+      <div>Bottom</div>
+    </Container>
+  );
 }
